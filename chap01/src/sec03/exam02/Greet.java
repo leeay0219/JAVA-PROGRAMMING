a727343af7183c499280ad7b5b2dcf95b5c1ccba
leeay0219 <leeay0219@gmail.com>
2019-07-17 09:33:49 +0900

    Day1

---
+++ chap01/src/sec03/exam02/Greet.java
@@ -0,0 +1,12 @@
+package sec03.exam02;
+
+public class Greet {
+	
+	public static void main(String[] args) {
+		
+		System.out.println ("안녕하세요");
+		System.out.println ("반갑");
+		System.out.println ("좋은하루");
+	}
+	
+}
---
+++ chap01/src/sec03/exam03/Board.java
@@ -0,0 +1,11 @@
+package sec03.exam03;
+
+public class Board {
+
+	public static void main(String[] args) {
+			System.out.println("-------------------------------");
+			System.out.println("1.목록 \t 2.생성 \t 3.보기 \t 4.삭제 \t 5.종료");
+			System.out.println("-------------------------------");
+	}
+
+}
